0f4368ad70e170ee95870a51aea36edb32c0e063
Brody <brodyover@gmail.com>
2024-06-11 16:09:14 -0400

    first

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+@app.route('/health')
+def health():
+    return jsonify({"health": "ok"})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
